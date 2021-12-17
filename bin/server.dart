c436cf1bf1890ef26fbf2853190baa65e546aa93
Santo Shakil <shakil.mezbah@gmail.com>
2021-12-17 19:25:15 +0600

    first commit

---
+++ bin/server.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf_web_socket/shelf_web_socket.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+Map<String, WebSocketChannel> _channels = {};
+
+final _router = Router()
+  ..get('/', _rootHandler)
+  ..get('/echo/<message>', _echoHandler)
+  ..get('/send/<token>', _webSocketHandler);
+
+Response _rootHandler(Request req) {
+  return Response.ok('Hello');
+}
+
+Response _echoHandler(Request request) {
+  return Response.ok('${request.params}\n');
+}
+
+FutureOr<Response> _webSocketHandler(Request request) async {
+  String? _token = request.params['token'];
+  if (_token == null) return Response.forbidden('Invalid token');
+
+  void _onConnection(WebSocketChannel webSocket) {
+    _channels.addAll({_token: webSocket});
+    print('Client($_token) connected!');
+    webSocket.stream.listen(
+      (message) => print('Received message: $message'),
+      onError: (error) => webSocket.sink.add(error),
+      onDone: () => _channels.remove(_token),
+      cancelOnError: false,
+    );
+    if (_token == '10001') {
+      for (var _channel in _channels.entries) {
+        _channel.value.sink.add('Hello, Client($_token)');
+      }
+    }
+  }
+
+  return await webSocketHandler(_onConnection)(request);
+}
+
+void main(List<String> args) async {
+  final ip = InternetAddress.anyIPv4;
+  final _handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final server = await serve(_handler, ip, port);
+
+  print('Server listening on port ${server.port}');
+}
