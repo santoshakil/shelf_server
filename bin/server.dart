d3f254868231680dc1d7318ebd8d5a43737629bf
Santo Shakil <shakil.mezbah@gmail.com>
2021-12-18 00:14:49 +0600

    ..

--- bin/server.dart
+++ bin/server.dart
@@ -23,7 +23,8 @@ FutureOr<Response> _webSocketHandler(Request request) async {
 
   void _onConnection(WebSocketChannel webSocket) {
     _channels.addAll({_token: webSocket});
-    print('Client($_token) connected!');
+    print(
+        '${DateTime.now().millisecondsSinceEpoch} Client($_token) connected!');
     webSocket.stream.listen(
       (m) => print('Received message: $m'),
       onError: (e) {
