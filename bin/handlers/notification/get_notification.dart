b0da7a84d6c293b1183a0ca6ff46f9effbcf70a7
Santo Shakil <shakil.mezbah@gmail.com>
2021-12-18 17:18:10 +0600

    handlers fixes

--- bin/handlers/notification/get_notification.dart
+++ bin/handlers/notification/get_notification.dart
@@ -1,4 +1,5 @@
 import 'dart:async' show FutureOr;
+import 'dart:convert' show jsonDecode;
 
 import 'package:shelf/shelf.dart' show Request, Response;
 import 'package:shelf_web_socket/shelf_web_socket.dart' show webSocketHandler;
@@ -6,24 +7,30 @@ import 'package:web_socket_channel/web_socket_channel.dart'
     show WebSocketChannel;
 
 import '../../database/channels.dart' show channels;
+import '../../models/user/user.dart';
 
 FutureOr<Response> getNotificationHandler(Request request) async {
-  String? _token = request.headers['Authorization'];
+  final String? _token = request.headers['Authorization'];
   if (_token == null) return Response.forbidden('Invalid token');
+  final String? _email = request.headers['email'];
+  if (_email == null) return Response.forbidden('email is required');
+  final User? _user = User.users.get(_email);
+  if (_user == null) return Response.forbidden('Invalid email');
+  if (_user.token != _token) return Response.forbidden('Invalid token');
 
   void _onConnection(WebSocketChannel webSocket) {
-    channels.addAll({_token: webSocket});
-    print('Client($_token) connected!');
+    channels.addAll({_email: webSocket});
+    print('${_user.name} is connected!');
     webSocket.sink.add('You are connected!');
-    webSocket.stream.listen(
-      (m) => print('Received message: $m'),
-      onError: (e) {
-        webSocket.sink.add(e);
-        print('Error: $e');
-      },
-      onDone: () => channels.remove(_token),
-      cancelOnError: false,
-    );
+    // webSocket.stream.listen(
+    //   (m) => print('message: $m'),
+    //   onError: (e) {
+    //     webSocket.sink.add(e);
+    //     print('Error: $e');
+    //   },
+    //   onDone: () => channels.remove(_token),
+    //   cancelOnError: false,
+    // );
   }
 
   return await webSocketHandler(_onConnection)(request);
--- bin/handlers/notification/send_notification.dart
+++ bin/handlers/notification/send_notification.dart
@@ -1,19 +1,33 @@
 import 'dart:async' show FutureOr;
+import 'dart:convert' show jsonDecode;
 
 import 'package:shelf/shelf.dart' show Request, Response;
 
 import '../../database/channels.dart' show channels;
+import '../../models/user/user.dart' show User;
 
 FutureOr<Response> sendNotificationHandler(Request request) async {
+  final String _body = await request.readAsString();
+  final Map<String, dynamic> _map = jsonDecode(_body);
+
   final String? _auth = request.headers['Authorization'];
   if (_auth == null) return Response.forbidden('Invalid Authorization');
+  final String? _message = _map['message'];
+  final String? _email = _map['email'];
+  final String? _to = _map['to'];
+  if (_message == null || _email == null || _to == null) {
+    return Response.forbidden('message, email and to are required');
+  }
 
-  final String? _token = request.headers['to'];
-  if (_token == null) return Response.forbidden('Invalid token');
+  final User? _user = User.users.get(_email);
+  if (_user == null) return Response.forbidden('User not found');
+  if (_user.token != _auth) return Response.forbidden('Invalid Authorization');
+  final User? _toUser = User.users.get(_to);
+  if (_toUser == null) return Response.forbidden('To user not found');
 
-  final _channel = channels[_token];
-  if (_channel == null) return Response.forbidden('User not found');
-  _channel.sink.add(await request.readAsString());
+  final _channel = channels[_to];
+  if (_channel == null) return Response.forbidden('To User is not connected');
+  _channel.sink.add(_message);
 
   return Response.ok('Message sent!');
 }
