341f16cbabb301db03ca13bedd1562a8c057ea2a
Santo Shakil <shakil.mezbah@gmail.com>
2021-12-18 13:16:18 +0600

    init hive

--- bin/database/hive.dart
+++ bin/database/hive.dart
@@ -1,1 +1,5 @@
+import 'package:hive/hive.dart' show Hive;
 
+Future<void> initHive() async {
+  Hive.init('.hive');
+}
--- bin/server.dart
+++ bin/server.dart
@@ -1,10 +1,13 @@
 import 'package:shelf/shelf.dart' show Pipeline, logRequests;
 import 'package:shelf/shelf_io.dart' show serve;
 
+import 'database/hive.dart' show initHive;
 import 'handlers/handlers.dart' show handlers;
 import 'helpers/constants.dart' show serverIP, serverPort;
 
 void main(List<String> args) async {
+  await initHive();
+
   final _handler = Pipeline().addMiddleware(logRequests()).addHandler(handlers);
   final server = await serve(_handler, serverIP, serverPort);
 
