db157dc34764a5a59250b222959bc28f450b8d21
Santo Shakil <shakil.mezbah@gmail.com>
2021-12-18 13:50:58 +0600

    user object

--- bin/database/hive.dart
+++ bin/database/hive.dart
@@ -1,12 +1,17 @@
 import 'package:hive/hive.dart' show Hive;
 
-import '../models/user/user.dart' show UserAdapter;
+import '../models/user/user.dart' show User, UserAdapter;
 
 Future<void> initHive() async {
   Hive.init('.hive');
   _registerAdapter();
+  await _openBox();
 }
 
 void _registerAdapter() {
   Hive.registerAdapter(UserAdapter());
+}
+
+Future<void> _openBox() async {
+  await Hive.openBox<User>(User.boxName);
 }
--- bin/models/user/user.dart
+++ bin/models/user/user.dart
@@ -1,11 +1,14 @@
+import 'dart:convert' show json;
+
 import 'package:hive/hive.dart'
     show
+        BinaryReader,
+        BinaryWriter,
+        Hive,
         HiveField,
         HiveObject,
         HiveType,
-        TypeAdapter,
-        BinaryReader,
-        BinaryWriter;
+        TypeAdapter;
 
 import '../../helpers/constants.dart' show hiveTypeUser;
 
@@ -13,6 +16,9 @@ part 'user.g.dart';
 
 @HiveType(typeId: hiveTypeUser)
 class User extends HiveObject {
+  static const String boxName = 'userBox';
+  Future<void> put() async => await Hive.box<User>(boxName).put(id, this);
+
   @HiveField(0)
   int id;
   @HiveField(1)
@@ -31,4 +37,59 @@ class User extends HiveObject {
     required this.password,
     required this.token,
   });
+
+  User copyWith({
+    int? id,
+    String? name,
+    String? email,
+    String? password,
+    String? token,
+  }) {
+    return User(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      password: password ?? this.password,
+      token: token ?? this.token,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
+      'token': token,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['id']?.toInt() ?? 0,
+      name: map['name'] ?? '',
+      email: map['email'] ?? '',
+      password: map['password'] ?? '',
+      token: map['token'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) => User.fromMap(json.decode(source));
+
+  @override
+  String toString() =>
+      'User(id: $id, name: $name, email: $email, password: $password, token: $token)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is User && other.id == id;
+  }
+
+  @override
+  int get hashCode =>
+      id.hashCode ^ name.hashCode ^ email.hashCode ^ password.hashCode;
 }
