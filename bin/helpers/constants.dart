6001b64d869e578aed96abfc22ac824b75ca120a
Santo Shakil <shakil.mezbah@gmail.com>
2022-02-14 14:34:13 +0600

    ..

--- bin/helpers/constants.dart
+++ bin/helpers/constants.dart
@@ -1,7 +1,6 @@
 import 'dart:io' show InternetAddress, Platform;
 
-// final serverIP = InternetAddress.anyIPv4;
-final serverIP = 'communication.algorithmgeneration.com';
+final serverIP = InternetAddress.anyIPv4;
 final serverPort = int.parse(Platform.environment['PORT'] ?? '8080');
 
 const int hiveTypeUser = 1;
