25d3ea041bc0c96dd218c83c009fa485fcfcb180
Santo Shakil <shakil.mezbah@gmail.com>
2022-02-14 18:38:05 +0600

    xx

--- bin/handlers/notification/send_notification.dart
+++ bin/handlers/notification/send_notification.dart
@@ -69,8 +69,11 @@ FutureOr<Response> sendNotificationHandler(Request request) async {
 
     return Response.ok('Message sent!');
   } on Exception catch (e) {
+    print('xxxxxxxxxxxxxxxxx');
     print(await request.readAsString());
+    print('xxxxxxxxxxxxxxxxx');
     print(e);
+    print('xxxxxxxxxxxxxxxxx');
     return Response.internalServerError(body: 'Internal Server Error');
   }
 }
