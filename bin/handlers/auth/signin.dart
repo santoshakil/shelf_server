8cb25b5c5244e9326733fbbb94056f5b307013c7
Santo Shakil <shakil.mezbah@gmail.com>
2022-02-14 18:39:21 +0600

    xx

--- bin/handlers/auth/signin.dart
+++ bin/handlers/auth/signin.dart
@@ -6,34 +6,43 @@ import 'package:shelf/shelf.dart' show Request, Response;
 import '../../models/user/user.dart' show User;
 
 FutureOr<Response> signInHandler(Request request) async {
-  String _body = await request.readAsString();
-  var _map = jsonDecode(_body);
-  String? _email = _map['email'];
-  String? _password = _map['password'];
+  try {
+    String _body = await request.readAsString();
+    var _map = jsonDecode(_body);
+    String? _email = _map['email'];
+    String? _password = _map['password'];
 
-  Map<String, String> _e = {};
-  if (_email == null) _e.addAll({'email': 'email is required'});
-  if (_password == null) _e.addAll({'password': 'password is required'});
-  if (_e.isNotEmpty) return Response.forbidden(_e.toString());
+    Map<String, String> _e = {};
+    if (_email == null) _e.addAll({'email': 'email is required'});
+    if (_password == null) _e.addAll({'password': 'password is required'});
+    if (_e.isNotEmpty) return Response.forbidden(_e.toString());
 
-  User? _user = User.users.get(_email);
-  if (_user == null) return Response.forbidden('user not found');
-  if (_user.password != _password) {
-    return Response.forbidden('password is incorrect');
+    User? _user = User.users.get(_email);
+    if (_user == null) return Response.forbidden('user not found');
+    if (_user.password != _password) {
+      return Response.forbidden('password is incorrect');
+    }
+    print('user: ${_user.name} logged in');
+
+    return Response.ok('''
+    {
+      "message": "user created",
+      "token": "${_user.token}",
+      "id": "${_user.id}",
+      "name": "${_user.name}",
+      "email": "${_user.email}",
+      "phone": "${_user.phone}",
+      "address": "${_user.address}",
+      "designation": "${_user.designation}",
+      "depertment": "${_user.depertment}"
+    }
+    ''');
+  } on Exception catch (e) {
+    print('xxxxxxxxxxxxxxxxx');
+    print(await request.readAsString());
+    print('xxxxxxxxxxxxxxxxx');
+    print(e);
+    print('xxxxxxxxxxxxxxxxx');
+    return Response.internalServerError(body: 'Internal Server Error');
   }
-  print('user: ${_user.name} logged in');
-
-  return Response.ok('''
-  {
-    "message": "user created",
-    "token": "${_user.token}",
-    "id": "${_user.id}",
-    "name": "${_user.name}",
-    "email": "${_user.email}",
-    "phone": "${_user.phone}",
-    "address": "${_user.address}",
-    "designation": "${_user.designation}",
-    "depertment": "${_user.depertment}"
-  }
-  ''');
 }
