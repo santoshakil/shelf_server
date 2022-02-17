3ef7d48e2340a96fb078b888e23f132093758d05
Santo Shakil <shakil.mezbah@gmail.com>
2022-02-17 14:16:07 +0600

    Active User List

--- bin/handlers/handlers.dart
+++ bin/handlers/handlers.dart
@@ -4,11 +4,12 @@ import 'auth/signin.dart' show signInHandler;
 import 'auth/signup.dart' show signUpHandler;
 import 'notification/get_notification.dart' show getNotificationHandler;
 import 'notification/send_notification.dart' show sendNotificationHandler;
-import 'user/user.dart' show getUserListHandler;
+import 'user/user.dart' show getActiveUserListHandler, getUserListHandler;
 
 final handlers = Router()
   ..post('/signup', signUpHandler)
   ..post('/signin', signInHandler)
   ..post('/sendNotificaion', sendNotificationHandler)
   ..get('/getNotificaion', getNotificationHandler)
-  ..get('/userList', getUserListHandler);
+  ..get('/userList', getUserListHandler)
+  ..get('/activeUserList', getActiveUserListHandler);
