acd2958385828c0030f008322e645fb13c4ac387
Santo Shakil <shakil.mezbah@gmail.com>
2022-02-21 17:56:52 +0600

    postgres WIP

--- bin/database/postgres.dart
+++ bin/database/postgres.dart
@@ -1,7 +1,11 @@
-import 'package:postgres/postgres.dart';
+import 'package:postgres/postgres.dart' show PostgreSQLConnection;
+
+import '../models/user/user.dart';
+
+late PostgreSQLConnection postgres;
 
 Future<void> openPostgres() async {
-  final postgres = PostgreSQLConnection(
+  postgres = PostgreSQLConnection(
     '0.0.0.0',
     5432,
     'postgres',
@@ -10,5 +14,55 @@ Future<void> openPostgres() async {
   );
 
   await postgres.open();
-  await postgres.execute('CREATE DATABASE IF NOT EXISTS users');
+
+  await postgres.execute(
+    '''
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL,
+      password VARCHAR(255) NOT NULL,
+      created_at TIMESTAMP NOT NULL DEFAULT NOW()
+    )
+    ''',
+  );
+}
+
+Future<List<User>> getUsers() async {
+  final results = await postgres.query(
+    '''
+    SELECT * FROM users
+    ''',
+  );
+
+  return results.map((row) => User.fromMap(row.toColumnMap())).toList();
+}
+
+Future<User?> getUserByEmail(String email) async {
+  final result = await postgres.mappedResultsQuery(
+    'SELECT * FROM users WHERE email = @email',
+    substitutionValues: {'email': email},
+  );
+
+  if (result.isEmpty) {
+    return null;
+  }
+
+  return User.fromMap(result.first);
+}
+
+Future<User> createUser(User user) async {
+  await postgres.execute(
+    '''
+    INSERT INTO users (name, email, password)
+    VALUES (@name, @email, @password)
+    ''',
+    substitutionValues: {
+      'name': user.name,
+      'email': user.email,
+      'password': user.password,
+    },
+  );
+
+  return user;
 }
--- bin/main.dart
+++ bin/main.dart
@@ -2,11 +2,13 @@ import 'package:shelf/shelf.dart' show Pipeline, logRequests;
 import 'package:shelf/shelf_io.dart' show serve;
 
 import 'database/hive.dart' show initHive;
+import 'database/postgres.dart' show openPostgres;
 import 'handlers/handlers.dart' show handlers;
 import 'helpers/constants.dart' show serverIP, serverPort;
 
 void main(List<String> args) async {
   await initHive();
+  await openPostgres();
 
   final _handler = Pipeline().addMiddleware(logRequests()).addHandler(handlers);
   final server = await serve(_handler, serverIP, serverPort);
