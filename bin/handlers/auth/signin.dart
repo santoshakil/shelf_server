68b6f2a2ea9957780c4ba4b204ae23423cff5b50
kakon007 <kakon443@gmail.com>
2022-03-03 12:11:29 +0600

    signIn API check

--- bin/handlers/auth/signin.dart
+++ bin/handlers/auth/signin.dart
@@ -3,7 +3,7 @@ import 'dart:convert' show jsonDecode;
 
 import 'package:shelf/shelf.dart' show Request, Response;
 
-import '../../models/user.dart' show User ;
+import '../../models/user.dart' show User;
 
 FutureOr<Response> signInHandler(Request request) async {
   try {
@@ -11,13 +11,18 @@ FutureOr<Response> signInHandler(Request request) async {
     var _map = jsonDecode(_body);
     String? _email = _map['email'];
     String? _password = _map['password'];
+     print('UserEmail ${_map['email']}');
+    print('UserPassword ${ _map['password']}');
+    print('UserEmail $_email');
+    print('UserPassword $_password');
 
     Map<String, String> _e = {};
     if (_email == null) _e.addAll({'email': 'email is required'});
     if (_password == null) _e.addAll({'password': 'password is required'});
     if (_e.isNotEmpty) return Response.forbidden(_e.toString());
 
-    User? _user = User.users.values.firstWhere((element) => element.email==_email);
+    User? _user =
+        User.users.values.firstWhere((element) => element.email == _email);
     if (_user == null) return Response.forbidden('user not found');
     if (_user.password != _password) {
       return Response.forbidden('password is incorrect');
